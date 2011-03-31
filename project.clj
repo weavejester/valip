8ff41790ced56d5c76648a7b13ee14db1a818566
James Reeves <jreeves@weavejester.com>
2011-03-31 11:01:00 -0400

    Released 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject valip "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+(defproject valip "0.1.0"
+  :description "Functional validation library"
+  :dependencies [[org.clojure/clojure "1.2.0"]])
