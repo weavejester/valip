afcbbfbcbcf35fcf76da4f9d468aa486ce647b7a
James Reeves <jreeves@weavejester.com>
2011-03-31 15:35:52 -0400

    Added email address validations

--- src/valip/predicates.clj
+++ src/valip/predicates.clj
@@ -3,6 +3,9 @@
   form."
   (:require [clojure.string :as string])
   (:import
+    java.util.Hashtable
+    javax.naming.NamingException
+    javax.naming.directory.InitialDirContext
     [org.apache.commons.validator.routines IntegerValidator
                                            DoubleValidator]))
 
@@ -17,19 +20,47 @@
   [re]
   (fn [s] (boolean (re-matches re s))))
 
+(defn email-address?
+  "Returns true if the email address is valid, based on RFC 2822. Email
+  addresses containing quotation marks or square brackets are considered
+  invalid, as this syntax is not commonly supported in practise. The domain of
+  the email address is not checked for validity."
+  [email]
+  (let [re (str "(?i)[a-z0-9!#$%&'*+/=?^_`{|}~-]+"
+                "(?:\\.[a-z0-9!#$%&'*+/=?" "^_`{|}~-]+)*"
+                "@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+"
+                "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]
+    (boolean (re-matches (re-pattern re) email))))
+
+(defn- dns-lookup [^String hostname ^String type]
+  (let [params {"java.naming.factory.initial"
+                "com.sun.jndi.dns.DnsContextFactory"}]
+    (try
+      (.. (InitialDirContext. (Hashtable. params))
+          (getAttributes hostname (into-array [type]))
+          (get type))
+      (catch NamingException _
+        nil))))
+
+(defn valid-email-domain?
+  "Returns true if the domain of the supplied email address has a MX DNS entry."
+  [email]
+  (let [domain (second (re-matches #".*@(.*)" email))]
+    (boolean (dns-lookup domain "MX"))))
+
 (defn integer-string?
   "Returns true if the string represents an integer."
   [s]
-  (boolean (. (IntegerValidator.) (validate s))))
+  (boolean (.validate (IntegerValidator.) s)))
 
 (defn decimal-string?
   "Returns true if the string represents a decimal number."
   [s]
-  (boolean (. (DoubleValidator.) (validate s))))
+  (boolean (.validate (DoubleValidator.) s)))
 
 (defn- parse-number [x]
   (if (string? x)
-    (. (DoubleValidator.) (validate x))
+    (.validate (DoubleValidator.) x)
     x))
 
 (defn gt
