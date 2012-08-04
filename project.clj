c00c2bb44fbeb012f2911fc7b41912011a4ef740
Michael Wu <mw10013@gmail.com>
2012-08-04 01:31:00 -0400

    Allow key to be function so a predicate can check more than one value in the map.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject valip "0.2.0"
   :description "Functional validation library"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-validator "1.3.1"]
-                 [clj-time "0.3.0"]])
+                 [clj-time "0.4.0"]])
